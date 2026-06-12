f80f03d58a46ecfc8bce71044f6f1d55077e2dc3
LuisGerardoOrtaRivera <I24050390@monclova.tecnm.mx>
2026-06-12 10:43:54 -0600

    Add files via upload

---
+++ Acertijo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcapeRoomDigitalPOO
+{
+    public abstract class Acertijo
+    {
+        public string Pregunta { get; set; }
+        public int Intentos { get; set; }
+        public string RespuestaCorrecta { get; set; }
+        public abstract bool ValidarRespuesta(string respuesta);
+    }
+}
